76ff11e55ab9a1aa168b8b61dd360e6c19cee901
cmiller12312 <130690370+cmiller12312@users.noreply.github.com>
2023-04-21 13:01:13 -0500

    docstrings

--- account.py
+++ account.py
@@ -1,22 +1,49 @@
 class Account():
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        '''
+        Initializes account
+        
+        :param name: account name
+        '''
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        '''
+        Deposits amount into account
+        
+        :param amount: amount you wihs to deposit
+        :return Will return True if successful and False if not
+        '''
         if amount > 0:
             self.__account_balance + amount
             return True
         return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        withdraws amount from account
+        
+        :param amount: amount you wish to withdraw
+        :return Will return True if successful and False if not
+        '''
         if amount > 0　and amount <= self.__account_balance:
             self.__account_balance - amount
             return True
         return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        returns the account balance
+        
+        :return account balance
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        returns the account name
+        
+        :return Account name
+        '''
         return self.__account_name
